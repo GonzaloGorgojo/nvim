f6b82c844d1eab226e04012f808b10ab7a2413a1
gon <gongorgojo@gmail.com>
2025-10-14 20:41:53 -0300

    updated ui for dap

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -119,13 +119,48 @@ return {
 				end,
 				desc = "Widgets",
 			},
+			{
+				"<leader>du",
+				function()
+					require("dapui").toggle()
+				end,
+				desc = "Toggle DAP UI",
+			},
+			vim.keymap.set("n", "<leader>da", function()
+				require("dapui").elements.watches.add()
+			end, { desc = "Add Watch Expression" }),
 		},
+
 		config = function()
 			local dap = require("dap")
 			local dapui = require("dapui")
 
 			-- Setup DAP UI
-			dapui.setup()
+			dapui.setup({
+				layouts = {
+					{
+						elements = {
+							{ id = "scopes", size = 0.33 },
+							{ id = "breakpoints", size = 0.33 },
+							{ id = "watches", size = 0.34 },
+						},
+						size = 25,
+						position = "left",
+					},
+					{
+						elements = {
+							-- remove "repl" to avoid that extra bottom pane
+							{ id = "console", size = 1 },
+						},
+						size = 10,
+						position = "bottom",
+					},
+				},
+				floating = {
+					border = "rounded",
+					mappings = { close = { "q", "<Esc>" } },
+				},
+			})
 
 			-- Setup virtual text
 			require("nvim-dap-virtual-text").setup()
@@ -134,12 +169,12 @@ return {
 			dap.listeners.after.event_initialized["dapui_config"] = function()
 				dapui.open()
 			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
+			-- dap.listeners.before.event_terminated["dapui_config"] = function()
+			-- 	dapui.close()
+			-- end
+			-- dap.listeners.before.event_exited["dapui_config"] = function()
+			-- 	dapui.close()
+			-- end
 
 			-- Configure Node.js adapter
 			dap.adapters["pwa-node"] = {
@@ -206,24 +241,29 @@ return {
 					skipFiles = { "<node_internals>/**" },
 					console = "integratedTerminal",
 				},
+			}
+
+			dap.configurations.typescript = {
 				{
 					type = "pwa-node",
 					request = "launch",
 					name = "NVIM Launch backend",
-					args = { "prisma dev" },
-					program = "${workspaceFolder}/src/main.ts",
+					program = "${workspaceFolder}/dist/main.js",
+					protocol = "inspector",
 					cwd = "${workspaceFolder}/src",
+					runtimeExecutable = "yarn",
+					console = "integratedTerminal",
+					args = { "dev" },
 					env = function()
 						local env_path = vim.fn.getcwd() .. "/.env"
 						return read_env_file(env_path)
 					end,
 					skipFiles = { "<node_internals>/**" },
-					console = "integratedTerminal",
 				},
 				{
 					type = "pwa-node",
 					request = "launch",
-					name = "Yarn Prisma Seed",
+					name = "NVIM backend Seed",
 					cwd = "${workspaceFolder}",
 					runtimeExecutable = "yarn",
 					runtimeArgs = { "prisma:seed" },
@@ -234,9 +274,6 @@ return {
 					end,
 				},
 			}
-
-			-- Also set up for TypeScript files
-			dap.configurations.typescript = dap.configurations.javascript
 		end,
 	},
 
